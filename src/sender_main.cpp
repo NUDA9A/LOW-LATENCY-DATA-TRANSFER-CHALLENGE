ced83e1102621bc832a6eb65ae1a09a896d8cc67
Akimenko Ivan <vlakimenko2004@mail.ru>
2026-08-12 15:39:33 +0300

    Remove Kernel backend. Gonna build DPDK backend only

---
+++ src/sender_main.cpp
@@ -0,0 +1,16 @@
+#include <lldt/config.hpp>
+
+
+using SenderConfig = transport::EndpointConfig;
+
+
+int main(const int argc, const char* argv[])
+{
+    const SenderConfig config = transport::parse_endpoint_config(argc, argv);
+    if (config.err != transport::StartupError::OK)
+    {
+        return 1;
+    }
+
+    return 0;
+}
