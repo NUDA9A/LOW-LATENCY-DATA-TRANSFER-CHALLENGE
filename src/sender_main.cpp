7f745dba3511cd6bfcee8c5426de561c243e7371
Akimenko Ivan <vlakimenko2004@mail.ru>
2026-08-18 15:24:56 +0300

    Sender main done

--- src/sender_main.cpp
+++ src/sender_main.cpp
@@ -20,6 +20,7 @@
 #include <optional>
 #include <csignal>
 #include <cerrno>
+#include <vector>
 
 
 namespace
@@ -42,23 +43,72 @@ namespace
         std::uint64_t tx_packets_unsent{};
     };
 
+    void printInputCounters(const transport::SenderInputCounters& counters)
+    {
+        std::fprintf(stdout, "SenderInputCounters:\n");
+        std::fprintf(stdout, "Frames read:\t%lu\n", counters.frames_read);
+        std::fprintf(stdout, "Bytes read:\t%lu\n", counters.bytes_read);
+        std::fprintf(stdout, "Empty polls:\t%lu\n", counters.empty_polls);
+        std::fprintf(stdout, "Lapped events:\t%lu\n", counters.lapped_events);
+        std::fprintf(stdout, "Lapped frames skipped:\t%lu\n", counters.lapped_frames_skipped);
+        std::fprintf(stdout, "Invalid frames:\t%lu\n", counters.invalid_frames);
+        std::fprintf(stdout, "Source gap events:\t%lu\n", counters.source_gap_events);
+        std::fprintf(stdout, "Source frames missing:\t%lu\n\n", counters.source_frames_missing);
+    }
+
+    void printDataCounters(const SenderDataCounters& counters)
+    {
+        std::fprintf(stdout, "SenderDataCounters:\n");
+        std::fprintf(stdout, "Frames offered to packetization:\t%lu\n", counters.frames_offered_to_packetization);
+        std::fprintf(stdout, "Mbuf alloc failures:\t%lu\n", counters.mbuf_alloc_failures);
+        std::fprintf(stdout, "Frames packetized:\t%lu\n", counters.frames_packetized);
+        std::fprintf(stdout, "Data packets built:\t%lu\n", counters.data_packets_built);
+        std::fprintf(stdout, "Data payload bytes:\t%lu\n", counters.data_payload_bytes);
+        std::fprintf(stdout, "Data sequences consumed:\t%lu\n", counters.data_sequences_consumed);
+        std::fprintf(stdout, "Tx packets enqueued:\t%lu\n", counters.tx_packets_enqueued);
+        std::fprintf(stdout, "Tx packets unsent:\t%lu\n\n", counters.tx_packets_unsent);
+    }
+
+    void printRteStats(const rte_eth_stats& counters)
+    {
+        std::fprintf(stdout, "RteStats:\n");
+        std::fprintf(stdout, "Successfully transmitted packets:\t%lu\n", counters.opackets);
+        std::fprintf(stdout, "Successfully transmitted bytes:\t%lu\n", counters.obytes);
+        std::fprintf(stdout, "Failed transmitted packets:\t%lu\n\n", counters.oerrors);
+    }
+
+    void printRteXstats(const rte_eth_xstat_name xstats_names[], const rte_eth_xstat xstats[], const int xstats_count)
+    {
+        std::fprintf(stdout, "RteXstats:\n");
+
+        for (int i = 0; i < xstats_count; ++i)
+        {
+            std::fprintf(stdout, "%s:\t%lu\n", xstats_names[i].name, xstats[i].value);
+        }
+    }
+
     int run_sender(const int argc, char* argv[])
     {
+        int err = 0;
+
         const auto configOpt = transport::try_parse_endpoint_config(argc, argv);
         if (!configOpt)
         {
+            std::fprintf(stderr, "[ERROR]: Could not parse EndpointConfig.\n");
             return 1;
         }
 
         const auto ena_port_info = dpdk::try_get_ena_port_info();
         if (!ena_port_info)
         {
+            std::fprintf(stderr, "[ERROR]: Could not get ena_port_info.\n");
             return 1;
         }
 
         dpdk::EnaTxPort port{ena_port_info->port_id};
         if (!port.try_initialize(ena_port_info->socket_id))
         {
+            std::fprintf(stderr, "[ERROR]: Could not initialize EnaTxPort.\n");
             return 1;
         }
 
@@ -80,7 +130,7 @@ namespace
         {
             if (getrandom(&session_id, sizeof(session_id), 0) != static_cast<ssize_t>(sizeof(session_id)))
             {
-                std::fprintf(stderr, "ERROR: Can not generate valid session_id.\n");
+                std::fprintf(stderr, "[ERROR]: Could not generate valid session_id.\n");
                 return 1;
             }
         }
@@ -134,8 +184,9 @@ namespace
             if (!canonical_output)
             {
                 rte_pktmbuf_free(mbuf);
-                std::fprintf(stderr, "ERROR: Could not reserve data packet.\n");
-                return 1;
+                std::fprintf(stderr, "[ERROR]: Could not reserve data packet.\n");
+                err = 1;
+                break;
             }
 
             transport::RawDataPacketBuilder builder{
@@ -197,7 +248,44 @@ namespace
             data_counters.tx_packets_enqueued++;
         }
 
-        return 0;
+        rte_eth_stats rte_stats{};
+
+        printInputCounters(reader.get_counters());
+        printDataCounters(data_counters);
+
+        if (rte_eth_stats_get(ena_port_info->port_id, &rte_stats) == 0)
+        {
+            printRteStats(rte_stats);
+        } else
+        {
+            std::fprintf(stderr, "[WARNING]: Could not retrieve rte_eth_stats\n");
+        }
+
+        const int xstats_count = rte_eth_xstats_get_names(ena_port_info->port_id, nullptr, 0);
+        if (xstats_count > 0)
+        {
+            std::vector<rte_eth_xstat_name> xstats_names(xstats_count);
+            std::vector<rte_eth_xstat> xstats(xstats_count);
+
+            if (const int res = rte_eth_xstats_get_names(ena_port_info->port_id, xstats_names.data(), xstats_count); res < 0 || res != xstats_count)
+            {
+                std::fprintf(stderr, "[WARNING]: Could not retrieve xstats_names\n");
+                return err;
+            }
+
+            if (const int res = rte_eth_xstats_get(ena_port_info->port_id, xstats.data(), xstats_count); res < 0 || res != xstats_count)
+            {
+                std::fprintf(stderr, "[WARNING]: Could not retrieve xstats values\n");
+                return err;
+            }
+
+            printRteXstats(xstats_names.data(), xstats.data(), xstats_count);
+        } else
+        {
+            std::fprintf(stderr, "[WARNING]: Could not retrieve xstats_count\n");
+        }
+
+        return err;
     }
 }
 
@@ -206,7 +294,7 @@ int main(int argc, char* argv[])
     int eal_argc{};
     if (eal_argc = rte_eal_init(argc, argv); eal_argc < 0)
     {
-        std::fprintf(stderr, "ERROR: EAL init failed: %s.\n", rte_strerror(rte_errno));
+        std::fprintf(stderr, "[ERROR]: EAL init failed: %s.\n", rte_strerror(rte_errno));
         return 1;
     }
 
@@ -219,15 +307,14 @@ int main(int argc, char* argv[])
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
 
-    if (sigaction(SIGTERM, &sa, nullptr) < 0)
+    if (sigaction(SIGTERM, &sa, nullptr) < 0 || sigaction(SIGINT, &sa, nullptr) < 0)
     {
-        std::fprintf(stderr, "ERROR: sigaction failed: %s.\n", strerror(errno));
-        return 1;
-    }
+        std::fprintf(stderr, "[ERROR]: sigaction failed: %s.\n", strerror(errno));
+        if (rte_eal_cleanup())
+        {
+            std::fprintf(stderr, "[ERROR]: Could not cleanup EAL.\n");
+        }
 
-    if (sigaction(SIGINT, &sa, nullptr) < 0)
-    {
-        std::fprintf(stderr, "ERROR: sigaction failed: %s.\n", strerror(errno));
         return 1;
     }
 
@@ -236,13 +323,13 @@ int main(int argc, char* argv[])
 
     if (sender_result)
     {
-        std::fprintf(stderr, "ERROR: Sender initialization failed.\n");
+        std::fprintf(stderr, "[ERROR]: Sender initialization failed.\n");
         return 1;
     }
 
     if (cleanup_result)
     {
-        std::fprintf(stderr, "ERROR: Could not cleanup EAL.\n");
+        std::fprintf(stderr, "[ERROR]: Could not cleanup EAL.\n");
         return 1;
     }
 
