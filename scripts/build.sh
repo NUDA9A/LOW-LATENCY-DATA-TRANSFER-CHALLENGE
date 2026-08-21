a872007f3573b0355cc4ce059cd074ff816597dd
Akimenko Ivan <vlakimenko2004@mail.ru>
2026-08-21 14:32:47 +0300

    update build.sh for building lldt_receiver and add --compact flag

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,19 @@ set -Eeuo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT="$(cd "$SCRIPT_DIR" && git rev-parse --show-toplevel)"
+CODEC="raw"
+
+if [[ $# -eq 1 ]]; then
+  if [[ $1 == "--compact" ]]; then
+    CODEC="compact"
+  else
+    echo "ERROR: unknown flag"
+    exit 1
+  fi
+elif [[ $# -gt 1 ]]; then
+  echo "ERROR: Usage: ./build.sh [--compact]"
+  exit 1
+fi
 
 export CC="${CC:-gcc}"
 export CXX="${CXX:-g++}"
@@ -19,6 +32,7 @@ cmake \
     -DBUILD_TESTING=OFF \
     -DCMAKE_CXX_COMPILER="${CXX}" \
     -DCMAKE_CXX_FLAGS="-march=native" \
+    -DLLDT_MESSAGE_PROFILE="${CODEC}" \
     -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON
 
-cmake --build "${BUILD_DIR}" --target lldt_sender
+cmake --build "${BUILD_DIR}" --target lldt_sender lldt_receiver
